2e56685b2e7d75a00f76ce3a5f6aadafcd70ef18
Sachin Mathew <41045024+sachinolickan@users.noreply.github.com>
2022-02-19 15:48:24 +0530

    Add files via upload
    Initial Commit

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Sachin Mathew on 26/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
